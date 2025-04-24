53721edf07c858de6c60a7f900e3277786edc842
pedrinwp7 <pvinnicius07@gmail.com>
2025-04-23 21:35:26 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-console.log((10 > 5) && (5 > 1));
-console.log((4 == 4) && (10 < 5));
-console.log((3 >= 2) && (2 <= 2));
-console.log((7 != 7) && (5 > 4));
-console.log((8 > 3)  && (3 < 10));
+console.log('resultado 1',(10 > 5) && (5 > 1));
+console.log('resultado 2',(4 == 4) && (10 < 5));
+console.log('resultado 3',(3 >= 2) && (2 <= 2));
+console.log('resultado 4',(7 != 7) && (5 > 4));
+console.log('resultado 5',(8 > 3)  && (3 < 10));
